c1ad08a9b4be2ded6532f2b1ef7b3f94a003567a
John Liu <johnyuhanliu@gmail.com>
2013-03-02 19:03:52 -0500

    Adding brew install script and small fix.

---
+++ install.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class GitTodo < Formula
+  homepage 'http://github.com/johnliu/git-todo'
+  url 'http://github.com/johnliu/git-todo.git', :tag => '0.1'
+  version '0.1'
+
+  head 'http://github.com/johnliu/git-todo.git'
+
+  depends_on 'ack' => :recommended
+
+  def patches
+    DATA
+  end
+
+  def install
+    bin.install 'git-todo'
+    bin.install 'git-todos'
+  end
+
+  def test
+    system "#{bin}/git-todo"
+    system "#{bin}/git-todos"
+    system 'git todo'
+    system 'git todos'
+  end
+end
